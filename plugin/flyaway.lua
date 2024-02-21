a46a14646e97c2ffcfb53870628bb50a2f48e0c3
Niclas Eich <niclas2102@gmail.com>
2024-02-21 10:56:10 +0100

    Added autocompletion and minor bugfix. Now pull/push is also a feature

--- plugin/flyaway.lua
+++ plugin/flyaway.lua
@@ -29,7 +29,7 @@ local function save_cache()
     end
 end
 
-function _G.sync_directory() 
+function _G.sync_directory(sync_type)
     load_cache()
 
     -- Get the list of directories in the current working directory
@@ -39,23 +39,20 @@ function _G.sync_directory()
     table.insert(directories, 1, '.')
 
     -- Create a list of options for the user
-    local options = {'Select a directory to sync:'}
+    local options = {'Select a directory to sync:', '1. Enter a directory manually'}
     for i, directory in ipairs(directories) do
         directories[i] = vim.fn.fnamemodify(directory, ':p')
-        table.insert(options, string.format('%d. %s', i, directories[i]))
+        table.insert(options, string.format('%d. %s', 1+i, directories[i]))
     end
-
     -- Ask the user to select a directory
     local selection = vim.fn.inputlist(options)
 
-    -- Check if the user made a valid selection
-    if selection < 1 or selection > #directories then
-        print('Invalid selection')
-        return
+    -- If the user selected the manual entry option, ask for the manual entry
+    if selection == 1 then
+        directory = vim.fn.input('Enter the directory to sync: ')
+    else
+        directory = directories[selection]
     end
-
-    -- Get the selected directory
-    local directory = directories[selection]
     _G.last_directory = directory -- Store the selected directory as the last used option
 
     -- Read the SSH config file and extract the hostnames
@@ -68,7 +65,7 @@ function _G.sync_directory()
     end
 
     -- Create a list of options for the user
-    options = {'Select a remote machine:', '1. Enter a remote manually'}
+    options = {'Select a remote:', '1. Enter a remote manually'}
     for i, host in ipairs(hosts) do
         table.insert(options, string.format('%d. %s', i + 1, host)) -- Note the i + 1 here
     end
@@ -77,41 +74,43 @@ function _G.sync_directory()
     selection = vim.fn.inputlist(options)
 
     -- Check if the user made a valid selection
-    if selection < 1 or selection > #hosts + 1 then
-        print('Invalid selection')
-        return
-    end
-
-    -- Get the selected remote machine
-    local host
     if selection == 1 then
-        local default_host = _G.last_host and string.format(' (default: %s)', _G.last_host) or ''
-        host = vim.fn.input('Enter the remote machine' .. default_host .. ': ')
+        host = vim.fn.input('Enter the target host: ')
     else
-        host = hosts[selection - 1] -- Note the selection - 1 here
+        host = hosts[selection]
     end
-    _G.last_host = host -- Store the selected host as the last used option
 
-    -- Ask the user to enter the target directory on the remote machine
-    local default_target_directory = _G.last_target_directory and string.format(' (default: %s)', _G.last_target_directory) or ''
-    local target_directory = vim.fn.input('Enter the target directory on the remote machine' .. default_target_directory .. ': ', _G.last_target_directory)
-    _G.last_target_directory = target_directory -- Store the entered target directory as the last used option
+    -- Get the target host and directory from the cache
+    local cached_target_directory = _G.sync_cache[directory] and _G.sync_cache[directory].target_directory or ""
 
-    -- Sync the directory to the remote machine
-    local rsync_command = string.format('rsync -avz %s %s:%s', directory, host, target_directory)
-    print("syncing...")
-    print("executing: ", rsync_command)
+    -- Get the target host and directory from the user
+    local target_directory = vim.fn.input('Enter the target directory: ', cached_target_directory)
+
+    -- Determine the rsync command based on the sync_type
+    local rsync_command
+    if sync_type == 'pull' then
+        rsync_command = string.format('rsync -avz %s:%s %s', host, target_directory, directory)
+    else -- default to 'push'
+        rsync_command = string.format('rsync -avz %s %s:%s', directory, host, target_directory)
+    end
+
     local result = os.execute(rsync_command)
 
     if result then
         print("Sync successful")
-        _G.last_host = host
-        _G.last_target_directory = target_directory
+        _G.sync_cache[directory] = {host = host, target_directory = target_directory}
         save_cache()
     else
         print("Sync failed")
     end
 end
 
+-- Create a Vim command that calls the sync_directory function
+vim.cmd([[
+    command! -nargs=1 -complete=customlist,SyncDirectoryComplete FlyAwaySync call luaeval('_G.sync_directory(_A)', <f-args>)
 
-vim.cmd('command! FlyAwaySyncToRemote lua _G.sync_directory()')
+    function! SyncDirectoryComplete(ArgLead, CmdLine, CursorPos)
+        let l:options = ['pull', 'push']
+        return filter(l:options, 'v:val =~ "^' . a:ArgLead . '"')
+    endfunction
+]])
